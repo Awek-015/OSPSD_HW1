637e72bdcacdfda191bdd0b63f732582cd62050a
XuanyuYang <73046573+XuanyuYang@users.noreply.github.com>
2025-02-12 00:07:37 +0800

    feat: Add component Calculator and unit tests

---
+++ src/calculator.py
@@ -0,0 +1,14 @@
+class Calculator:
+    """Calculator - Performs basic arithmetic operations."""
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def subtract(a, b):
+        return a - b
+
+    @staticmethod
+    def multiply(a, b):
+        return a * b
