503a02517d8f27a4e49010ce33b50c9021545aee
Haoran Zhou <hz3575@nyu.edu>
2025-03-02 22:33:39 -0500

    修复项目构建配置和 CircleCI 配置

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+"""
+Calculator-Logger package.
+This package provides a calculator with logging capabilities.
+"""
+
+from src.calculator import Calculator
+from src.logger import Logger
+from src.notifier import Notifier
+
+__all__ = ["Calculator", "Logger", "Notifier"]
